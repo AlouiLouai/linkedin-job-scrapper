89025d47596a4ef0e4654df2e4ed20313966f22b
alouilouai <louaialoui1993@gmail.com>
2025-01-22 19:01:12 +0100

    fixin the page issue

--- client/src/app/api/fetch_jobs/route.ts
+++ client/src/app/api/fetch_jobs/route.ts
@@ -1,18 +1,13 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-
-  // Extract query parameters for pagination
-  const page = searchParams.get("page") || "1";
-  const per_page = searchParams.get("per_page") || "10";
 
   // Parse the JSON body from the request
   const body = await req.json();
 
   try {
     // Make the POST request to the Flask backend
-    const response = await fetch(`https://linkedin-job-scrapper.onrender.com/fetch_jobs?page=${page}&per_page=${per_page}`, {
+    const response = await fetch(`https://linkedin-job-scrapper.onrender.com/fetch_jobs`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
